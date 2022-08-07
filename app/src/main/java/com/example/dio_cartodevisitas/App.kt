cc9b5cacdb54bcde971afd8029ecc3771b13a39a
Mauro Crispim <mcrispim@hotmail.com>
2022-08-07 18:37:37 -0300

    Cria variaveis globais (App) database e repository

--- app/src/main/java/com/example/dio_cartodevisitas/App.kt
+++ app/src/main/java/com/example/dio_cartodevisitas/App.kt
@@ -1,6 +1,10 @@
 package com.example.dio_cartodevisitas
 
 import android.app.Application
+import com.example.dio_cartodevisitas.data.AppDatabase
+import com.example.dio_cartodevisitas.data.CartaoVisitaRepo
 
 class App: Application() {
+    val database: AppDatabase by lazy { AppDatabase.getDatabase(this) }
+    val repository: CartaoVisitaRepo by lazy { CartaoVisitaRepo(database.cartaoVisitasDao()) }
 }
