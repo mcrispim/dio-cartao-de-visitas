6cf60daca594ea5a387381a054318db2538df3cd
Mauro Crispim <70303414+mcrispim@users.noreply.github.com>
2022-08-08 14:34:20 -0300

    Conecta MainActivity ao MainViewModel

--- app/src/main/java/com/example/dio_cartodevisitas/ui/MainActivity.kt
+++ app/src/main/java/com/example/dio_cartodevisitas/ui/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.dio_cartodevisitas.ui
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
+import com.example.dio_cartodevisitas.App
 import com.example.dio_cartodevisitas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(LayoutInflater.from(this)) }
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
