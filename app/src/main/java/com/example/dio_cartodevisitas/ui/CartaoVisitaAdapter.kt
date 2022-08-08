9d4ef83ffeaa67f0bca5b9fedb68b791fb13c815
Mauro Crispim <70303414+mcrispim@users.noreply.github.com>
2022-08-08 15:31:01 -0300

    Cria CartaoVisitaAdapter

---
+++ app/src/main/java/com/example/dio_cartodevisitas/ui/CartaoVisitaAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.dio_cartodevisitas.ui
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dio_cartodevisitas.data.CartaoVisitas
+import com.example.dio_cartodevisitas.databinding.ItemCartaoVisitasBinding
+import java.util.zip.Inflater
+
+class CartaoVisitaAdapter: ListAdapter<CartaoVisitas, CartaoVisitaAdapter.ViewHolder> (DiffCallback()){
+
+    val listenerShare: (View) -> Unit = {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemCartaoVisitasBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class ViewHolder(private val itemBinding: ItemCartaoVisitasBinding): RecyclerView.ViewHolder(itemBinding.root) {
+
+        fun bind(item: CartaoVisitas) {
+            itemBinding.tvNome.text = item.nome
+            itemBinding.tvTelefone.text = item.telefone
+            itemBinding.tvEmpresa.text = item.empresa
+            itemBinding.tvEmail.text = item.email
+            itemBinding.mcvContent.setBackgroundColor(Color.parseColor(item.fundoPersonalizado))
+            itemBinding.mcvContent.setOnClickListener {
+                listenerShare(it)
+            }
+        }
+    }
+
+}
+
+class DiffCallback: DiffUtil.ItemCallback<CartaoVisitas>() {
+
+    override fun areItemsTheSame(oldItem: CartaoVisitas, newItem: CartaoVisitas) = oldItem == newItem
+
+    override fun areContentsTheSame(oldItem: CartaoVisitas, newItem: CartaoVisitas) = oldItem.id == newItem.id
+
+}
--- app/src/main/java/com/example/dio_cartodevisitas/ui/MainActivity.kt
+++ app/src/main/java/com/example/dio_cartodevisitas/ui/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.dio_cartodevisitas.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.viewModels
+import com.example.dio_cartodevisitas.AddCartaoActivity
 import com.example.dio_cartodevisitas.App
+import com.example.dio_cartodevisitas.data.CartaoVisitas
 import com.example.dio_cartodevisitas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +20,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        getAllCartaoVisita()
+        insertListener()
+    }
+
+    fun insertListener() {
+        binding.fabAdd.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddCartaoActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    fun getAllCartaoVisita() {
+        mainViewModel.getAll().observe(this, { cards ->
+
+        })
     }
 }
